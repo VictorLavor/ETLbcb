a4f409302b1444d77f9dfb9f9a0c2bd88faadf34
Victor Matheus de Lavor <150476865+VictorLavor@users.noreply.github.com>
2025-03-21 16:07:19 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import pandas as pd
+
+
+
+def requestApiBcb(data):
+    """
+    Funcão para extrair os dados dos meios de pagamentos trimestrais do Banco Central.
+
+    parâmentros:
+    data - string - aaaat (Exemplo: 20191)
+    """
+    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json"
+    
+    req = requests.get(url)
+    dados = req.json()
+    
+    df = pd.json_normalize(dados['value'])
+    return print(df)
+requestApiBcb('20241')
+
