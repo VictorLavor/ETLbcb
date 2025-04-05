48ac2a78de6bed6ae475736bdac0582b31d552e8
Victor Matheus de Lavor <150476865+VictorLavor@users.noreply.github.com>
2025-04-05 17:30:47 -0300

    Segunda Aula

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.extractTransform import etlBcB
+from src.load import saveCsv
 
 df = etlBcB('20191')
+<<<<<<< HEAD
 print(df)
 
 # salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";",".")
@@ -8,3 +10,7 @@ print(df)
 # salvarSqlite(dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
 
 salvarMySql(dadosBcb, "root", "root", "localhost", "etlbcb", "meios_pagamento_tri")
+=======
+
+saveCsv(df, "meiosPagamentosTri", ";", ".")
+>>>>>>> a83fbb7 (Segunda Aula)
