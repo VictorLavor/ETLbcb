cb01030e7c5290c724165a431f9354dcd8803a96
Victor Matheus de Lavor <150476865+VictorLavor@users.noreply.github.com>
2025-04-11 15:30:21 -0300

    Atualizando com banco de dados

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from src.extractTransform import etlBcB
-from src.load import saveCsv
+import pandas as pd
+from src.extractTransform import requestsApiBcb
+from src.load import salvarCSV, salvarSQLite, salvarMySQL
 
-df = etlBcB('20191')
-<<<<<<< HEAD
-print(df)
+dadosBcb = requestsApiBcb("20191")
+salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";", ".")
 
-# salvarCsv(dadosBcb, "src/datasets/meiosPagamentosTri.csv", ";",".")
+salvarSqlite(dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
 
-# salvarSqlite(dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
-
-salvarMySql(dadosBcb, "root", "root", "localhost", "etlbcb", "meios_pagamento_tri")
-=======
-
-saveCsv(df, "meiosPagamentosTri", ";", ".")
->>>>>>> a83fbb7 (Segunda Aula)
+salvarMySQL(dadosBcb, "root", "root", "localhost", "etlbcb", "meiospagamentostri")
--- src/load.py
+++ src/load.py
@@ -1,9 +1,34 @@
 import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
 
-def saveCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal:str):
-    df.to_csv(f'src/datasets/{nome_arquivo}.csv', decimal=decimal, sep=separador)
+
+def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal: str):
+    df.to_csv(f"src/datasets/{nome_arquivo}.csv", decimal=decimal, sep=separador)
+    return
+
+
+def salvarSQLite(df: pd.DataFrame, nome_banco: str, nome_tabela: str):
+
+    conn = sqlite3.connect(nome_banco)
+
+    df.to_sql(nome_tabela, conn, if_exists="replace", index=False)
+
+    conn.close()
+    return
+
+
+def salvarMySQL(
+    df: pd.DataFrame, usuario: str, senha: str, host: str, banco: str, nome_tabela: str
+):
+    engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@{host}/{banco}")
+    df.to_sql(nome_tabela, con=engine, if_exists="replace", index=False)
+
+    return
+
 
 from pathlib import Path
+
 
 def salvarCSV(df, nome_arquivo):
     """
